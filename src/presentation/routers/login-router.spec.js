f4a50fe8a83abceea949d77d1695d9f1f5b41dcc
Marcos Vinicius <marvn.ants1@gmail.com>
2020-07-24 09:35:05 -0300

    feat: ensure loginRouter returns 500 if an invalid httpRequest is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,11 @@
 const loginRoute = () => {
   return {
     route(httpRequest) {
+      if (!httpRequest || !httpRequest.body) {
+        return {
+          statusCode: 500,
+        };
+      }
       const { email, password } = httpRequest.body;
       if (!email || !password) {
         return {
@@ -14,17 +19,18 @@ const loginRoute = () => {
 describe('Login Route', () => {
   test('Should return 400 if no email is provided', () => {
     const sut = loginRoute();
+
     const httpRequest = {
       body: {
         password: 'any_password',
       },
     };
+
     const httpResponse = sut.route(httpRequest);
+
     expect(httpResponse.statusCode).toBe(400);
   });
-});
 
-describe('Login Route', () => {
   test('Should return 400 if no password is provided', () => {
     const sut = loginRoute();
     const httpRequest = {
@@ -33,6 +39,14 @@ describe('Login Route', () => {
       },
     };
     const httpResponse = sut.route(httpRequest);
+
     expect(httpResponse.statusCode).toBe(400);
+  });
+
+  test('Should return 500 if httpResponse has no body', () => {
+    const sut = loginRoute();
+    const httpResponse = sut.route({});
+
+    expect(httpResponse.statusCode).toBe(500);
   });
 });
