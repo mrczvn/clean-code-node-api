d3634e300bbf70bcd58114ee7a0d9afe01789256
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-05 21:25:47 -0300

    feat: ensure emailValidator returns false if validartor returns false

--- src/app/services/email-validator.spec.js
+++ src/app/services/email-validator.spec.js
@@ -1,7 +1,9 @@
+const validator = require('validator');
+
 const emailValidator = () => {
   return {
     isValid: (email) => {
-      return true;
+      return validator.isEmail(email);
     },
   };
 };
@@ -13,5 +15,15 @@ describe('Email Validator', () => {
     const isEmailValid = sut.isValid('valid_email@mail.com');
 
     expect(isEmailValid).toBe(true);
+  });
+
+  test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false;
+
+    const sut = emailValidator();
+
+    const isEmailValid = sut.isValid('invalid_email@mail.com');
+
+    expect(isEmailValid).toBe(false);
   });
 });
