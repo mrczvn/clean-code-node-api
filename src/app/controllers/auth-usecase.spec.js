c01e58c36cf344e42326c9a4b57c53fbbec0d70b
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-07 10:24:35 -0300

    feat: ensure authUseCase throws if an invalid load loadUserByEmailRepository is provided

--- src/app/controllers/auth-usecase.spec.js
+++ src/app/controllers/auth-usecase.spec.js
@@ -1,4 +1,4 @@
-const { missingParamError } = require('../../util');
+const { missingParamError, invalidParamError } = require('../../util');
 
 const authUseCase = (loadUserByEmailRepository) => {
   return {
@@ -6,6 +6,12 @@ const authUseCase = (loadUserByEmailRepository) => {
       if (!email) return missingParamError('email');
 
       if (!password) return missingParamError('password');
+
+      if (!loadUserByEmailRepository)
+        return missingParamError('loadUserByEmailRepository');
+
+      if (!loadUserByEmailRepository.load)
+        return invalidParamError('loadUserByEmailRepository');
 
       const response = await loadUserByEmailRepository.load(email);
 
@@ -51,11 +57,37 @@ describe('Auth UseCase', () => {
   test('Should call loadUserByEmailRepository with correct email', async () => {
     const { sut } = makeSut();
 
-    const res = await sut.auth({
+    const isValidated = await sut.auth({
       email: 'any_email@mail.com',
       password: 'any_password',
     });
 
-    expect(res).toBe('any_email@mail.com');
+    expect(isValidated).toBe('any_email@mail.com');
+  });
+
+  test('Should throw if no loadUserByEmailRepository is provided', async () => {
+    const sut = authUseCase();
+
+    const promise = sut.auth({
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+
+    expect(promise).rejects.toThrow(
+      missingParamError('loadUserByEmailRepository')
+    );
+  });
+
+  test('Should throw if loadUserByEmailRepository has no load method', async () => {
+    const sut = authUseCase({});
+
+    const promise = sut.auth({
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+
+    expect(promise).rejects.toThrow(
+      invalidParamError('loadUserByEmailRepository')
+    );
   });
 });
