f5d18f7a693c05d232a5b3c1f9b4cc37235d4291
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-16 17:10:46 -0300

    feat: ensure requests return content-type as JSON by default

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,7 +1,9 @@
 const cors = require('../middlewares/cors');
 const jsonParser = require('../middlewares/json-parser');
+const contentType = require('../middlewares/content-type');
 
 module.exports = (app) => {
   app.use(cors);
   app.use(jsonParser);
+  app.use(contentType);
 };
