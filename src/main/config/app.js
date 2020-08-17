d9fd8bec5735e0bb4d0b16f68bcb1058f38bc16b
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-17 10:25:19 -0300

    chore: add routes file

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const setupApp = require('./setup');
+const setupRoutes = require('./routes');
 
 const app = express();
 
 setupApp(app);
+setupRoutes(app);
 
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-module.exports = app
+module.exports = app;
