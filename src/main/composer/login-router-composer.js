994e5e1393e3765cab17e61fb830a8e44ccae2bf
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-17 13:27:48 -0300

    chore: move loginRouter to its own file

---
+++ src/main/composer/login-router-composer.js
@@ -0,0 +1,24 @@
+const LoginRouter = require('../../presentation/routes/login-router');
+const AuthUseCase = require('../../domain/usecases/auth-usecase');
+const emailValidator = require('../../presentation/services/email-validator');
+const loadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository');
+const updateAccessTokenRepository = require('../../infra/repositories/update-access-token-repository');
+const encrypter = require('../../presentation/services/encrypter');
+const tokenGenerator = require('../../presentation/services/token-generator');
+const { tokenSecret } = require('../config/env');
+
+tokenGenerator(tokenSecret);
+
+const authUseCase = AuthUseCase({
+  loadUserByEmailRepository: loadUserByEmailRepository(),
+  updateAccessTokenRepository: updateAccessTokenRepository(),
+  encrypter: encrypter(),
+  tokenGenerator: tokenGenerator(tokenGenerator),
+});
+
+const loginRouter = LoginRouter({
+  authUseCase,
+  emailValidator: emailValidator(),
+});
+
+module.exports = loginRouter;
--- src/main/routes/login-router.js
+++ src/main/routes/login-router.js
@@ -1,12 +1,5 @@
-// const loginRouter = require('../../presentation/routes/login-router');
-// const authUseCase = require('../../domain/usecases/auth-usecase');
-// const emailValidator = require('../../presentation/services/email-validator');
-// const loadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository');
-// const updateAccessTokenRepository = require('../../infra/repositories/update-access-token-repository');
-// const encrypter = require('../../presentation/services/encrypter');
-// const tokenGenerator = require('../../presentation/services/token-generator');
-// const {tokenSecret} = require('../config/env');
+const loginRouter = require('../composer/login-router-composer');
 
-// module.exports = (router) => {
-//   router.post('/login');
-// };
+module.exports = (router) => {
+  router.post('/login', loginRouter);
+};
