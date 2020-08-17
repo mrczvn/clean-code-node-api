16baee434acb4127045ca0abd43089776ad1b3d2
Marcos Vinicius <marvn.ants1@gmail.com>
2020-08-17 09:44:25 -0300

    feat: connect to mongodb before listen for app

---
+++ src/main/config/env.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoUrl:
+    process.env.MONGO_URL ||
+    'mongodb+srv://marvn:tthsv@cluster-jest.oyeep.mongodb.net/mangoclean?retryWrites=true&w=majority',
+};
--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,14 @@
-const app = require('./config/app');
+const App = require('./config/app');
+const mongoHelper = require('../infra/helpers/mongo-helper');
+const env = require('./config/env');
 
-app.listen(8000, () => console.log(`Server is running`));
+const setupApp = (app) => {
+  mongoHelper
+    .connect(env.mongoUrl)
+    .then(() => {
+      app.listen(8000, () => console.log(`Server is running`));
+    })
+    .catch(console.error);
+};
+
+setupApp(App);
