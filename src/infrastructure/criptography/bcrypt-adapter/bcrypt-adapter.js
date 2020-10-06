89d1b89d1a0098fc53d9061b1fcfe0decd96a984
Marcos Vinicius <marvn.ants1@gmail.com>
2020-10-06 19:19:43 -0300

    test: ensure BcryptAdapter.compare return true when compare succeeds

--- src/infrastructure/criptography/bcrypt-adapter/bcrypt-adapter.js
+++ src/infrastructure/criptography/bcrypt-adapter/bcrypt-adapter.js
@@ -1,7 +1,10 @@
 const bcrypt = require('bcrypt')
 
 const bcryptAdapter = (salt) => ({
-  hash: async (plaintext) => await bcrypt.hash(plaintext, salt)
+  hash: async (plaintext) => await bcrypt.hash(plaintext, salt),
+
+  compare: async ({ plaintext, digest }) =>
+    await bcrypt.compare(plaintext, digest)
 })
 
 module.exports = bcryptAdapter
